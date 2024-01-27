f6e1b1dbd98ae2fd7b777e31484bba24e9290df2
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-01-27 10:19:51 +0530

    Update yt_dl.py

--- plugins/ExtraMods/yt_dl.py
+++ plugins/ExtraMods/yt_dl.py
@@ -43,7 +43,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [Jot](https://t.me/netflixvilla002)**"
+        cap = "**BY›› [𝐍𝐞𝐭𝐟𝐥𝐢𝐱 𝐕𝐢𝐥𝐥𝐚 𝐈𝐧𝐝𝐢𝐚](https://t.me/netflixvilla002)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
